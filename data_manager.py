6800f0bd4ce5faf3a000910e78218854ce9b8a15
bezdomnij <bajni37@gmail.com>
2020-05-06 20:10:30 +0200

    modified data_manager / deleted unused code (get_questioner)

--- data_manager.py
+++ data_manager.py
@@ -316,24 +316,3 @@ def answer_number_by_user(cursor: RealDictCursor, user_id):
             WHERE user_id = %(user_id)s"""
     cursor.execute(query, {'user_id': user_id})
     return cursor.fetchall()
-
-#
-# @database_common.connection_handler
-# def get_questioner(cursor: RealDictCursor, answer_id):
-#     query = """
-#             SELECT question.user_id
-#             FROM question inner join answer
-#                 on question.id = answer.question_id
-#             WHERE answer.id = %(a_id)s;"""
-#     cursor.execute(query, {'a_id': answer_id})
-#     return cursor.fetchall()
-
-
-@database_common.connection_handler
-def get_questioner(cursor: RealDictCursor, question_id):
-    query = """ SELECT users.id FROM users
-                INNER JOIN question q on users.id = q.user_id
-                WHERE q.id = %(quid)s"""
-    cursor.execute(query, {'quid': question_id})
-    return cursor.fetchone()
-
